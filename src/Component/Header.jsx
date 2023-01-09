6c99a3e3268ec93485a5717b2f61c7f37f3c59a8
DOMEMY <konton07@hotmail.com>
2023-01-09 18:49:26 +0700

    Update to use useReducer.

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,12 +1,13 @@
-import React, {useEffect, useState, useCallback} from 'react'
+import React, {useEffect, useState, useReducer} from 'react'
 import Ipinfo from './Ipinfo'
 import Searching from './Searching'
 import Map from './Map';
 
 
 export default function Header() {
-    const [isSearch, setIsSearch] = useState(false);
-    const [userIp, setUserIp] = useState("");
+    // const [isSearch, setIsSearch] = useState(false);
+    // const [userIp, setUserIp] = useState("");
+    const url= "https://geo.ipify.org/api/v2/country,city?apiKey=at_4I8EuUR3buRJxvbw2lWD6jlkfikcm&ipAddress="
     const [ipDetail, setIpDetail] = useState({
         ip:"",
         lat:"",
@@ -16,25 +17,40 @@ export default function Header() {
         isp:""
     });
 
-    const searchHandler= (address)=>{
-        setUserIp(address);
+    const urlReducer = (state, action)=>{
+        //set type อีก type
+        console.log(state);
+        if(action.type ==='IP_ADDRESS'){
+            return {value: action.val, isSearch: true, isClick: !state.isClick};
+        }
+        return {value: '', isSearch: true, isClick: state.isClick};
+    }
 
-        // fetchIp();
+    const [fetchUrl, dispatchFetchUrl] = useReducer(urlReducer, {
+        value:'',
+        isSearch: false,
+        isClick: false
+    })
+
+
+    
+
+    const searchHandler= (address)=>{
+        dispatchFetchUrl({type: 'IP_ADDRESS', val: address})
+        // setUserIp(address);
     }
     
     useEffect(()=>{
-        fetchIp()
-        console.log("FETCH");
-    },[userIp]);
+        fetchIp(url+fetchUrl.value)
+        console.log("FETCH USE EFFECT"); 
+    },[fetchUrl.isClick]);
 
 
-    const fetchIp = async()=>{
-        const url = "https://geo.ipify.org/api/v2/country,city?apiKey=at_4I8EuUR3buRJxvbw2lWD6jlkfikcm&ipAddress="+userIp;
-        console.log(url);
-        const response = await fetch(url)
-        console.log(response);
+    const fetchIp = async(address)=>{
+        const response = await fetch(address);
         const data = await response.json();
         const {ip, isp, location} = data
+        
         setIpDetail({
             ip: ip,
             lat: location.lat,
@@ -43,8 +59,8 @@ export default function Header() {
             timeZone: location.timezone,
             isp: isp
         });
-        console.log(ipDetail);
-        setIsSearch(true);   
+        dispatchFetchUrl({type: 'DEFAULT', val: 'https://geo.ipify.org/api/v2/country,city?apiKey=at_4I8EuUR3buRJxvbw2lWD6jlkfikcm&ipAddress='});
+        // setIsSearch(true);   
     };
 
     
@@ -56,7 +72,7 @@ export default function Header() {
                 <Searching search={searchHandler}/>
             </div>
             <Ipinfo address={ipDetail}/>
-            {isSearch &&  <Map lat={ipDetail.lat} lng={ipDetail.lng}/>}
+            {fetchUrl.isSearch?  <Map lat={ipDetail.lat} lng={ipDetail.lng} zoom={12}/>: <Map lat={49} lng={2} zoom={2}/> }
            
         </div>
     )
--- src/Component/Map.jsx
+++ src/Component/Map.jsx
@@ -2,18 +2,18 @@ import React, {useState, useEffect} from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 export default function Map(props) {
-    console.log(props);
-    const {lat,lng} = props
+    const {lat,lng, zoom} = props
     const [cordinates , setCordinates] = useState(props);
 
     useEffect(() => {
+        // console.log("Trigger");
         setCordinates(props);
-    }, [props])
+    }, [cordinates]);
 
   return (
  
     <MapContainer key={lat}center={[lat,
-            lng]} zoom={12}scrollWheelZoom={true} style={{zIndex:-1, top:-85}}>
+            lng]} zoom={zoom}scrollWheelZoom={true} style={{zIndex:-1, top:-85}}>
     <TileLayer
   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
